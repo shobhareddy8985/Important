f65e6e497a64bc8d3ee011d75411876266c29eed
shobhareddy <shobha.reddy@diyotta.com>
2019-11-15 16:01:45 +0530

    my pythone local files

---
+++ python programs/myfirstsamplefile.py
@@ -0,0 +1,74 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "##### My first program in python\n",
+    "sample program\n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "hello world\n",
+      "hello world\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"hello world\")\n",
+    "print('hello world')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "hi hello\n"
+     ]
+    }
+   ],
+   "source": [
+    "a = \"hi\"\n",
+    "b =' '\n",
+    "c =\"hello\"\n",
+    "d = a+b+c\n",
+    "print(d)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
